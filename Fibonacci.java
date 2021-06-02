562b86f9a5050c21761e2c4d383f0f3ba72ee7e9
Uday bhargav <85021896+udaybhargav23@users.noreply.github.com>
2021-06-02 22:41:44 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -16,3 +16,19 @@ class Main{
         return res;
     }
 }
+
+
+
+/////Bottom up (Tabulation)
+/*
+public static int fibonacciBottomUpDp(int n, int dp[]){
+		
+		dp[0] = 0;
+		dp[1] = 1;
+		for(int i=2; i<=n; i++){
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+		
+		return dp[n];
+	}
+*/
